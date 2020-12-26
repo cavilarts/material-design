c0f3b4f7e8ee49652d3999ee7ffa52d0eb2b9e39
Carlos Avila <carlos.avila@globant.com>
2020-12-26 14:53:59 -0500

    adding files

---
+++ src/atoms/Button/Button.test.tsx
@@ -0,0 +1,10 @@
+import {shallow} from 'enzyme';
+import Button from './index';
+
+describe('Button', () => {
+  it('Should render a button primary by default', () => {
+    const button = shallow(<Button></Button>);
+
+    expect(button).toMatchSnapshot();
+  });
+});
---
+++ src/atoms/Button/index.tsx
@@ -0,0 +1,13 @@
+export interface buttonProps {
+  type?: 'primary' | 'secondary';
+}
+
+const Button:React.FC<buttonProps> = ({type}) => {
+  return (
+    <button>
+      
+    </button>
+  )
+}
+
+export default Button
